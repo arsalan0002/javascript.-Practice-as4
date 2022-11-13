83d88774a02dbed157023a0ae67695b99f97fad3
Muhammad_Arsalan Iqbal <116029293+arsalan0002@users.noreply.github.com>
2022-11-13 23:27:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+////Alert
+// alert("Error! Please enter a valid password.");
+// alert('Welcome to JS Land...\nHappy Coading!');
+// alert("Welcome to JS Land...");
+// alert("Happy Coading!");
+// alert("Hello..I can run JS through my web browser's Console");
+
+////Variable
+// var username = "Ali";
+// var myname = "Ali";
+
+// alert(username);
+// alert(myname);
+
+// var message = "Hello World !";
+// alert(message);
+
+// var Name = ("Ali");
+// var age = ("15 year old");
+// var cer = ("Certified Mobile Application Development");
+
+// alert(Name);
+// alert(age);
+// alert(cer);
+
+// var mes = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(mes);
+
+// var email = "ali1@gmail.com"
+// alert("My Eamil address is"  +  email);
